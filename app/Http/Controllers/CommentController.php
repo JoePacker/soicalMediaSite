9d66a1fb47d45a75f4254e56092f1abfd0d9b9a0
Joe Packer <joepacker99@gmail.com>
2019-12-01 21:27:43 +0000

    Creating api routes for comment resource

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,27 +3,48 @@
 namespace App\Http\Controllers;
 
 use App\Comment;
+use App\Post;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
     /**
-     * Show the form for creating a new resource.
+     * Display a listing of the resource.
      *
+     * @param  \App\Post  $post
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function index(Post $post)
     {
-        //
+        return $post->comments;
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Post  $post
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, Post $post)
+    {
+        $comment = new Comment();
+        $comment->user_id = $request->user()->id;
+        $comment->body = $request->body;
+
+        $post->comments()->save($comment);
+
+        return $comment;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Comment  $comment
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Comment $comment)
     {
         //
     }
@@ -31,11 +52,14 @@ class CommentController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Comment  $comment
+     * @param \App\Comment $comment
      * @return \Illuminate\Http\Response
+     * @throws \Exception
      */
     public function destroy(Comment $comment)
     {
-        //
+        $comment->delete();
+
+        return response()->json(null, 204);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('comments', 'CommentController')->only([
+    'update', 'destroy',
+]);
+
+Route::get('/posts/{post}/comments', 'CommentController@index');
+
+Route::post('/posts/{post}/comments', 'CommentController@store');
