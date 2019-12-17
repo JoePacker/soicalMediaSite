9160646412cbfb90a3b46fde7bb3dce575014918
Joe Packer <joepacker99@gmail.com>
2019-12-17 22:55:14 +0000

    Renaming app to hangout

--- public/js/app.js
+++ public/js/app.js
@@ -50115,8 +50115,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/vagrant/sites/ridealong/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/vagrant/sites/ridealong/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/vagrant/sites/hangout/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/vagrant/sites/hangout/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
